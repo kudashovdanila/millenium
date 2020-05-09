fd40a72fd206c6e19fc9723a00c42571b1e095cb
Dank <kudashovdanila@gmail.com>
2020-05-09 21:43:23 +0300

    Сделал попап

--- js/main.js
+++ js/main.js
@@ -1,6 +1,20 @@
 $(document).ready(function () {
+
   $(".menu-burger").click(function () { 
     $(this).toggleClass("menu-burger_active");
     $(".navbar-menu").toggleClass("navbar-menu_active");
+    $("body").toggleClass("lock");
   });
+
+  $('.input-phone').mask('+7 (000) 000-00-00');
+
+  $(".popup-link").click(function (e) { 
+    e.preventDefault();
+    $(".popup-wrap").fadeIn();
+  });
+
+  $(".popup-close").click(function () {
+    $(".popup-wrap").fadeOut();
+  });
+
 });
