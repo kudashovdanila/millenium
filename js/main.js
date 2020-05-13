30b4482962fad83dd59caa117a615b62bd46535c
Dank <kudashovdanila@gmail.com>
2020-05-13 14:14:36 +0300

    Настроил меню

--- js/main.js
+++ js/main.js
@@ -17,4 +17,22 @@ $(document).ready(function () {
     $(".popup-wrap").fadeOut();
   });
 
+  jQuery(function ($) {
+    $(document).mouseup(function (e) { // отслеживаем событие клика по веб-документу
+      var block = $(".popup"); // определяем элемент, к которому будем применять условия (можем указывать ID, класс либо любой другой идентификатор элемента)
+      if (!block.is(e.target) // проверка условия если клик был не по нашему блоку
+        && block.has(e.target).length === 0) { // проверка условия если клик не по его дочерним элементам
+        $(".popup-wrap").fadeOut(); // если условия выполняются - скрываем наш элемент
+      }
+    });
+  });
+
+  $(".hero").click(function () {
+    if ($(".navbar-menu").hasClass("navbar-menu_active")) {
+      $(".navbar-menu").toggleClass("navbar-menu_active");
+      $(".menu-burger").toggleClass("menu-burger_active");
+      $("body").toggleClass("lock");
+    }
+  });
+
 });
